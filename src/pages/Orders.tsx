b76677ee11847a5fdb35e9534bc04261a3915ac1
mukky254 <muhidinyusuf06@gmail.com>
2025-11-06 15:38:43 +0300

    Update Orders.tsx

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { ordersAPI } from '../services/api';
 import { Order } from '../types';
 import { 
@@ -8,7 +8,6 @@ import {
   Clock, 
   XCircle,
   Search,
-  Filter,
   Eye
 } from 'lucide-react';
 import toast from 'react-hot-toast';
